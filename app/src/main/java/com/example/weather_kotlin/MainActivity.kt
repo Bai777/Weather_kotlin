3067a196f124ad882b8e6ade3a7c025faf359949
Stepan <bai_777@mail.ru>
2021-07-29 11:59:31 +0300

    Приступил к дз 2 котлин

---
+++ app/src/main/java/com/example/weather_kotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.weather_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weather_kotlin.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
